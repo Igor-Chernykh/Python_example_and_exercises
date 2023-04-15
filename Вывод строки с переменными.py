82f5ee90c14bbaac7014393a52c1737395e5f219
Igor-Chernykh <123569595+Igor-Chernykh@users.noreply.github.com>
2023-04-15 18:19:51 +0300

    2 example

---
+++ Вывод строки с переменными.py
@@ -0,0 +1,4 @@
+username = input("Введите имя\n")
+foodname = input("Введите название еды\n")
+filmname = input("Введите название фильма\n")
+print("Привет,", username, "давай возьмем", foodname, "и посмотрим", filmname)
