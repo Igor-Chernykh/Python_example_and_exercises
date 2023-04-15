93833fc946328d555dd4e5afdcff34904bcc7b9d
Igor-Chernykh <123569595+Igor-Chernykh@users.noreply.github.com>
2023-04-15 20:07:15 +0300

    add

---
+++ if-else(animals).py
@@ -0,0 +1,9 @@
+animal = input("What's your animal?\n")
+if animal == "cat":
+    print("Feed the cat!")
+elif animal == "dog":
+    print("Feed the dog!")
+elif animal == "fish":
+    print("Feed the fish!")
+else:
+    print("Unknown animal. Type correct")
