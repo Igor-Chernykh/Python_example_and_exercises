4df0bcbaaa3df617165e5a4ba0df9d5fdc0255c8
Igor-Chernykh <123569595+Igor-Chernykh@users.noreply.github.com>
2023-04-19 13:33:50 +0300

    add

---
+++ Examples/Список list-set-tuple.py
@@ -0,0 +1,12 @@
+names = ["name1", "name2", "name3", "name3", "name4", "name5"]  # list
+print(names)
+for m in names:
+    print(m)
+# длина len() = 5
+# нумерация с 0
+# ''''''
+mas2 = set(names)  # set - преобразует список в набор уникальных данных, повторения удаляются
+print(mas2)
+
+mas3 = (1, 2, 3, 4)  # tuple - неизменяемый список. работает на поиск быстрее обычного
+print(mas3)
--- Examples/Список list.py
+++
@@ -1,5 +0,0 @@
-# names = [name1, name2, name3, name4, name5]  # list
-# ''''''
-# длина len() = 5
-# нумерация с 0
-# ''''''
