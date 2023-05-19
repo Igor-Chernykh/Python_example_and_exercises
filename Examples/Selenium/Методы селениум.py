14ff4efa0134e433e5ef76fe841628b820a9012c
Igor-Chernykh <123569595+Igor-Chernykh@users.noreply.github.com>
2023-05-19 23:49:35 +0300

    add

--- Examples/Selenium/Методы селениум.py
+++ Examples/Selenium/Методы селениум.py
@@ -1,18 +1,19 @@
 import random
-from selenium import webdriver      # импортируем веб-драйвер
 import time
+from selenium import webdriver      # импортируем веб-драйвер
 
-user_agents_list = [            # список юзер агентов
+user_agents_list = [                # список юзер агентов
     "hello_world",
     "best_of_the best",
     "some_user_agent"
 ]
 
 # options
-options = webdriver.FirefoxOptions()    # создание объекта опций
-# options.add_argument("user-agent=HelloWorld")  # добавление аргумента
+options = webdriver.FirefoxOptions()                                    # создание объекта опций
+# options.add_argument("user-agent=HelloWorld")                         # добавление аргумента
 options.add_argument(f"user-agent={random.choice(user_agents_list)}")   # рандомный выбор юзер-агента
-driver = webdriver.Firefox(options=options)      # настройки браузера
+# options.add_argument("--proxy-server = 138.128.91.65:8000")           # задать прокси
+driver = webdriver.Firefox(options=options)                             # настройки браузера
 
 
 # driver.get("")      # открывается сайт в инете
@@ -28,16 +29,17 @@ driver = webdriver.Firefox(options=options)      # настройки брауз
 
 
 try:
-    driver.get(url="https://www.instagram.com")     # получить данные
-    time.sleep(5)           # задержка времени
-    driver.refresh()        # обновить страницу
+    driver.get(url="https://www.instagram.com")         # получить данные
+    time.sleep(5)                                       # задержка времени
+    driver.refresh()                                    # обновить страницу
     time.sleep(2)
-    driver.get_screenshot_as_file("1.png")  # получить скриншот
+    driver.get_screenshot_as_file("1.png")              # получить скриншот
     driver.save_screenshot("2.png")
     time.sleep(2)
 
-except Exception as ex:     # обработать исключения
+except Exception as ex:                                 # обработать исключения
     print(ex)
+
 finally:
-    driver.close()      # закрыть драйвер
+    driver.close()                                      # закрыть драйвер
     driver.quit()
